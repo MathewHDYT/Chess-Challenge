138ec4951a8ac1d09c323926db3d4d5e06b9d7d8
MathewHDYT <48954742+MathewHDYT@users.noreply.github.com>
2023-07-21 18:50:06 +0200

    Implement inital Think implementation

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,66 @@
 ﻿using ChessChallenge.API;
+using System;
+using System.Linq;
+using System.Numerics;
+using System.Collections.Generic;
 
 public class MyBot : IChessBot
 {
+    private int [] pieceValues = {
+        0,      // Null
+        100,    // Pawn
+        325,    // Knight
+        325,    // Bishop
+        500,    // Rook
+        975,    // Queem
+        10000   // King
+    };
+
     public Move Think(Board board, Timer timer)
     {
-        Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        Move[] allMoves = board.GetLegalMoves();
+
+        // Pick a random move to play if nothing better is found
+        Random rng = new();
+        Move moveToPlay = allMoves[rng.Next(allMoves.Length)];
+        int highestValueCapture = 0;
+
+        foreach (Move move in allMoves)
+        {
+            // Check wheter the current move would result in a checkmate or not
+            board.MakeMove(move);
+            bool isMate = board.IsInCheckmate();
+            board.UndoMove(move);
+
+            // Always play checkmate in one
+            if (isMate)
+            {
+                moveToPlay = move;
+                break;
+            }
+
+            // Find highest value capture
+            int currentPieceValue = pieceValues[(int)move.MovePieceType];
+            int capturedPieceValue = pieceValues[(int)move.CapturePieceType];
+
+            int promotionPieceValue = 0;
+            if (move.IsPromotion) {
+                promotionPieceValue = pieceValues[(int)move.PromotionPieceType] - currentPieceValue;
+            }
+
+            int lossOnCapture = 0;
+            if (move.IsEnPassant) {
+                lossOnCapture = currentPieceValue;
+            }
+
+            int totalMoveValue = capturedPieceValue + promotionPieceValue - lossOnCapture;
+
+            if (totalMoveValue > highestValueCapture)
+            {
+                moveToPlay = move;
+                highestValueCapture = totalMoveValue;
+            }
+        }
+        return moveToPlay;
     }
 }
